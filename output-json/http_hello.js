7da57fa15f0f8ce3bbc433c12c1f31568bb5b3cd
takayuki-okazawa <okazawa@ohauetgoawr.local>
2013-04-22 20:19:47 +0900

    Edit File

--- output-json/http_hello.js
+++ output-json/http_hello.js
@@ -2,8 +2,16 @@ var http  = require('http');
 
 var server = http.createServer(function(req,res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Node!<h1>');
+    res.end(getData());
 });
 
 server.listen(8080);
 console.log('server started on 8080');
+
+
+function getData(){
+
+	str = "{hoge:'hogehoge',hoge2:'homhom'}";
+	return str;
+	
+}
