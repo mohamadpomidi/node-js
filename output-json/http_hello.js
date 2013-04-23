333c20d50c2a519ef3de2bd3777878adaf5b258a
takayuki-okazawa <okazawa@ohauetgoawr.local>
2013-04-23 18:59:11 +0900

    Edit File, Add File

--- output-json/http_hello.js
+++ output-json/http_hello.js
@@ -1,7 +1,22 @@
+//http://localhost:8080/?hoge=eeee
+
 var http  = require('http');
 
 var server = http.createServer(function(req,res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
+
+    //
+    getRequest(req.url);
+
+    //
+    req.on('data', function(chunk){
+        //postRequest(chunk);
+        var urlinfo = require('url').parse('/?'+chunk.toString(), true);
+        //var jsonObj = JSON.parse(urlinfo);
+        console.log(urlinfo);
+    });
+
+    //
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(getData());
 });
 
@@ -9,9 +24,49 @@ server.listen(8080);
 console.log('server started on 8080');
 
 
+
+
+
+
+
+/*******************
+ * Private Method
+ *******************/
+
 function getData(){
 
-	str = "{hoge:'hogehoge',hoge2:'homhom'}";
-	return str;
-	
+    str = "{hoge:'hogehoge',hoge2:'homhom'}";
+    return str;
+
+}
+
+function getRequest(url){
+
+    console.log(url);
+
+}
+
+function postRequest(chunk){
+
+    /*
+
+     { protocol: null,
+     slashes: null,
+     auth: null,
+     host: null,
+     port: null,
+     hostname: null,
+     hash: null,
+     search: '?a=abcd&b=1234',
+     query: { a: 'abcd', b: '1234' },
+     pathname: '/',
+     path: '/?a=abcd&b=1234',
+     href: '/?a=abcd&b=1234' }
+
+     */
+
+    var urlinfo = require('url').parse('/?'+chunk.toString(), true);
+    var jsonObj = JSON.parse(urlinfo);
+    console.log(urlinfo);
+
 }
